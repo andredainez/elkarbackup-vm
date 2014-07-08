cb3d9f0923a29d0b328d93005dc412c08182e142
Xabi Ezpeleta <xezpeleta@gmail.com>
2014-07-09 00:08:39 +0200

    Added support for multiple distros and multiple output formats

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
 
-boxname="elkarbackup-vm.box"
+DISTRO="wheezy"
+BOXNAME="elkarbackup-vm"
+IMGDIR="./images"
 
-echo "Creating and provisioning VM..."
+echo "Creating and provisioning $BOXNAME over $DISTRO..."
+
+d=$(pwd)
+cd $d/src/$DISTRO
 up=`vagrant up`
 
 echo "Up=$up"
@@ -14,16 +19,53 @@ fi
 
 if [ "$up" ];then
     echo "VM created and provisioned succesfully"
-    echo "Creating box package..."
-    mkdir build && cd build
-    vagrant package --output $boxname
-    echo "Unpacking the box..."
-    tar -xf $boxname
 
+    if [ ! -d "$d/$IMGDIR" ]; then
+      mkdir $d/$IMGDIR
+      mkdir $d/$IMGDIR/vagrant
+      mkdir $d/$IMGDIR/virtualbox
+      mkdir $d/$IMGDIR/vmware
+    else
+      if [ ! -d "$d/$IMGDIR/vagrant" ];then
+        mkdir $d/$IMGDIR/vagrant
+      fi
+
+      if [ ! -d "$d/$IMGDIR/kvm" ];then
+        mkdir $d/$IMGDIR/kvm
+      fi
+
+      if [ ! -d "$d/$IMGDIR/virtualbox" ];then
+        mkdir $d/$IMGDIR/virtualbox
+      fi
+
+      if [ ! -d "$d/$IMGDIR/vmware" ];then
+        mkdir $d/$IMGDIR/vmware
+      fi
+    fi
+
+    echo "Creating box package..."
+    vagrant package --output $d/$IMGDIR/vagrant/$BOXNAME.box
+    echo "Unpacking the box..."
+    tar -xf $d/$IMGDIR/vagrant/$BOXNAME.box -C $d/$IMGDIR/
+    # VMWare
+    cp $d/$IMGDIR/box-disk1.vmdk $d/$IMGDIR/vmware/box-disk1.vmdk
+    # KVM raw (veeeery slooow)
+    #VBoxManage clonehd $d/$IMGDIR/box-disk1.vmdk $d/$IMGDIR/kvm/$BOXNAME.raw --format RAW
+    #qemu-img convert -O raw $d/$IMGDIR/box-disk1.vmdk $d/$IMGDIR/kvm/$BOXNAME.raw
+    # VirtualBox
+    mv $d/$IMGDIR/box-disk1.vmdk $d/$IMGDIR/virtualbox/box-disk1.vmdk
+    mv $d/$IMGDIR/box.ovf $d/$IMGDIR/virtualbox/$BOXNAME.ovf
+
+    rm -f $d/$IMGDIR/Vagrantfile
     echo "Box unpacked. Removing vagrant vm..."
     vagrant destroy --force
-    echo "If it returns an error, try to do it manually: 'vagrant destroy --force'"
-    echo "Finished!"
+    #echo "If it returns an error, try to do it manually: 'vagrant destroy --force'"
+    echo "Finished! Summary:"
+    echo "\n"
+    # Summary
+    cd $d/$IMGDIR/
+    pwd
+    du -sh *
 else
     echo "ERROR: unknown error"
 fi
