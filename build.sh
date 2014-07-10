150d5c3520d2f79207e1294803bf3e93b7373506
Xabi Ezpeleta <xezpeleta@gmail.com>
2014-07-10 17:05:38 +0200

    Same var name in bootstrap

--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
 #! /bin/sh
 
-pwd=$(pwd)
-export PATH=$PATH:$pwd/packer
+d=$(pwd)
+export PATH=$PATH:$d/packer
 
-if packer validate elkarbackup.json; then
+if [ -d "output-vm" ];then
+    echo "WARNING: output-vm directory will be deleted!"
+    rm -RI output-vm
+
+    if [ -d "output-vm" ]; then
+        echo "File not deleted. Exiting..."
+        exit
+    fi
+fi
+
+
+if packer validate elkarbackup-wheezy.json; then
     packer build elkarbackup-wheezy.json
 else
     "Incorrect json configuration file"
